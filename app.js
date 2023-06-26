1cae2613dbac3e25b25d5c2293652a3516dbe92e
Mike Boyle <mikesboyle@gmail.com>
2023-06-26 11:50:41 -0400

    added /students using JSON data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Init the app and define route handlers, etc. here
 const express = require('express');
+const studentsData = require('./studentsData.json');
 
 const app = express();
 
@@ -7,6 +8,16 @@ const app = express();
 // app.get takes two args: path and handler func
 app.get('/', (request, response) => {
   response.status(200).json({ data: 'Service is running' });
+});
+
+// GET /students
+app.get('/students', (request, response) => {
+  try {
+    const { students } = studentsData;
+    response.status(200).json({ data: students });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = app;
